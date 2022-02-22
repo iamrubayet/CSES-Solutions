c3345d0730d62d4611ef143c64a3cf11e67d6f27
rubayet170746 <rafirubayet75@gmail.com>
2022-02-22 22:34:44 +0600

    added 1's update

--- Introductory Problems/Weird Algorithm.cpp
+++ Introductory Problems/Weird Algorithm.cpp
@@ -24,3 +24,16 @@ int main()
     cout << 1 <<endl;
     return 0;
 }
+
+/*The space complexity of this solution is
+O(1)
+. It is harder to find a time complexity since we don't even know if it always converges to
+1
+ for any given
+N
+. All we know (and you can test this) is that up to
+10
+6
+, the largest possible sequence you can get has size
+525
+. */
