e089b8b7558bc40810bbc23c1c1690cdbd2eaf11
rubayet170746 <rafirubayet75@gmail.com>
2022-02-23 00:44:01 +0600

    added 2

--- Introductory Problems/Missing Number.cpp
+++ Introductory Problems/Missing Number.cpp
@@ -1,16 +1,32 @@
+#include <iostream>
 #include <bits/stdc++.h>
+
 
 using namespace std;
 
-int N, x, xum;
 
-int main(){
-    scanf("%d", &N);
-    for(int i = 1; i <= N; i++)
-        xum ^= i;
-    for(int i = 0; i < N-1; i++){
-        scanf("%d", &x);
-        xum ^= x;
+int main()
+{
+    long long n;
+    long long sum1=0;
+    long long sum2=0;
+    long long x;
+    cin >> n;
+    for(int i=1;i<=n;i++){
+            sum2 +=i;
+
     }
-    printf("%d\n", xum);
+    for(int i=0;i<n-1;i++){
+
+        cin >> x;
+        sum1 +=x;
+    }
+
+    cout << sum2-sum1 <<endl;
+    return 0;
 }
+
+/* space  complexity 0(1) + 0(1) */
+
+
+/* time complexity 0(n) + 0(n) */
