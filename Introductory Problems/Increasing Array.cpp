d42f870fabe95601c092312cf5a2f61f99f33635
rubayet170746 <rafirubayet75@gmail.com>
2022-02-23 01:31:00 +0600

    added 4

--- Introductory Problems/Increasing Array.cpp
+++ Introductory Problems/Increasing Array.cpp
@@ -1,18 +1,30 @@
+#include <iostream>
 #include <bits/stdc++.h>
 
+
 using namespace std;
-typedef long long ll;
 
-int N;
-ll x, mx, sum;
 
-int main(){
-    scanf("%d", &N);
-    scanf("%lld", &mx);
-    for(int i = 1; i < N; i++){
-        scanf("%lld", &x);
-        mx = max(mx, x);
-        sum += (mx - x);
+int main()
+{
+    long long n;
+    long long ans=0;
+    cin >> n;
+    long long a[n+1];
+    for(int i=0;i<n;i++){
+        cin >> a[i];
     }
-    printf("%lld\n", sum);
+
+    for(int i=0;i<n-1;i++){
+        if(a[i]>a[i+1])
+        {
+            ans += abs(a[i]-a[i+1]);
+            a[i+1]=a[i];
+
+        }
+    }
+    cout << ans <<endl;
+    return 0;
 }
+
+/* time complexity is o(n) + o(n) */
