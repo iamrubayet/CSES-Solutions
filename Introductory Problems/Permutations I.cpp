0f3d380432ea9a2b813fd07eb95a33789dcfba84
rubayet170746 <rafirubayet75@gmail.com>
2022-04-16 14:13:51 +0600

    added 4th

--- Introductory Problems/Permutations I.cpp
+++ Introductory Problems/Permutations I.cpp
@@ -1,17 +1,31 @@
+#include <iostream>
 #include <bits/stdc++.h>
+using ll = long long;
+using ld = long double;
+
 
 using namespace std;
 
-int N;
 
-int main(){
-    scanf("%d", &N);
-    if(N == 1)      printf("1 ");
-    else if(N <= 3) printf("NO SOLUTION\n");
-    else {
-        for(int i = 2; i <= N; i += 2)
-            printf("%d ", i);
-        for(int i = 1; i <= N; i += 2)
-            printf("%d ", i);
+int main()
+{
+    long long n;
+    cin >> n;
+    if(n==1){
+        cout <<"1"<<endl;
     }
+    else if(n<=3){
+        cout <<"NO SOLUTION"<<endl;
+    }
+    else{
+        for(int i=2;i<=n;i+=2){
+                cout << i << " ";
+
+        }
+        for(int i=1;i<=n;i+=2){
+            cout <<i << " ";
+        }
+        cout <<endl;
+    }
+    return 0;
 }
