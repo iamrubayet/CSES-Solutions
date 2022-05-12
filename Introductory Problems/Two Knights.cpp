b8897f5a4652d346de3dcca61406d3959cf18fd7
rubayet170746 <rafirubayet75@gmail.com>
2022-05-13 03:32:11 +0600

    added

--- Introductory Problems/Two Knights.cpp
+++ Introductory Problems/Two Knights.cpp
@@ -1,15 +1,19 @@
+#include <iostream>
 #include <bits/stdc++.h>
+using ll = long long;
+using ld = long double;
+
 
 using namespace std;
-typedef long long ll;
 
-int N;
 
-int main(){
-    scanf("%d", &N);
-    for(int k = 1; k <= N; k++){
-        ll cnt = 1LL + (k-1)*(k-2)/2;
-        cnt = cnt * (k-1) * (k+4);
-        printf("%lld\n", cnt);
+int main()
+{
+    int n;
+    cin >> n;
+    for(long long k=1;k<=n;k++){
+        cout << (k*k * (k*k-1))/2 - 4*(k-1)*(k-2) <<endl;
     }
+    return 0;
 }
+
